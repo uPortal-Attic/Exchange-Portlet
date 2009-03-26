15b8b917b8d844ea48ec55b71fee3b97cbbb1d33
Charles Frank <cpfrank@uvic.ca>
2009-03-26 23:55:33 +0000

    Cleaning up code and addingin javadoc.
    git-svn-id: https://revision.uvic.ca/svn/WEBS_Portal/vender/uvic/trunk/source/portlets/ecsPortlet@38 a475a96e-93ce-11dd-808f-d46df4bc7295

--- src/main/java/ca/uvic/portal/ecsPortlet/domain/EcsSoap.java
+++ src/main/java/ca/uvic/portal/ecsPortlet/domain/EcsSoap.java
@@ -2,7 +2,6 @@ package ca.uvic.portal.ecsPortlet.domain;
 
 import org.apache.commons.httpclient.Credentials;
 import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.HttpException;
 import org.apache.commons.httpclient.NTCredentials;
 import org.apache.commons.httpclient.auth.AuthScope;
 import org.apache.commons.httpclient.methods.PostMethod;
@@ -19,29 +18,61 @@ import java.net.URL;
 import org.xml.sax.SAXException;
 
 /**
- * A simple domain shell
- * 
+ * A class that is responsible for marshalling a soap call and sending it to
+ * the Exchange server, parsing the soap response, and building domain objects.
+ * @author cpfrank
+ * @version svn:$Id$
+ *
  */
 public class EcsSoap {
     // Begin Spring injection params
+    /**
+     * private String exchangeURL Spring constructor injection.
+     */
     private static String exchangeURL;
+    /**
+     * private String exchangeUser Spring constructor injection.
+     */
     private static String exchangeUser;
+    /**
+     * private String exchangePassword Spring constructor injection.
+     */
     private static String exchangePassword;
+    /**
+     * private String exchangeDomain Spring constructor injection.
+     */
     private static String exchangeDomain;
-    private static String soapXML;
+    /**
+     * private String digesterRuleFile Spring constructor injection.
+     */
     private static String digesterRuleFile;
+    /**
+     * private String soapXML soap request xml from digesterRuleFile.
+     */
     // End Spring Injection Params
-    
-
+    private static String soapXML;
+    /**
+     * private commons logger.
+     */
     private final Log logger = LogFactory.getLog(getClass());
 
     //Use constructor injection
-    public EcsSoap(String url,
-                   String user, 
-                   String password,
-                   String domain, 
-                   EcsRemoteSoapCall soapCall,
-                   String digesterFile) {
+    /**
+     * Constructor uses Spring injection.
+     * @param url the resource located for the exchange server to query
+     * @param user the exchange user
+     * @param password the exchange user password
+     * @param domain the exchange domain
+     * @param soapCall the EcsRemoteCall soapCall type
+     * @param digesterFile the Digester rule file for mapping soap response to
+     *  an object
+     */
+    public EcsSoap(final String url,
+                   final String user,
+                   final String password,
+                   final String domain,
+                   final EcsRemoteSoapCall soapCall,
+                   final String digesterFile) {
 
         exchangeURL        = url;
         exchangeUser       = user;
@@ -49,12 +80,19 @@ public class EcsSoap {
         exchangeDomain     = domain;
         soapXML            = soapCall.getSoapCall();
         digesterRuleFile   = digesterFile;
-        
+
     }
 
-
-    public void queryExchange()
-      throws HttpException, IOException, SAXException {
+    /**
+     * Runs the marshalled soap query against the exchange server.
+     * Method is responsible for collecting credentials, marshalling a soap
+     * request call, querying the exchange server, and creating an stack of
+     * domain objects according to a Digester file specification.
+     * @throws IOException
+     * @throws SAXException
+     * @see org.apache.commons.digester.xmlrules.DigesterLoader
+     */
+    public final void queryExchange() throws IOException, SAXException {
 
         HttpClient client = new HttpClient();
         Credentials credentials = new NTCredentials(
@@ -68,18 +106,17 @@ public class EcsSoap {
         // Allow method declaration IOException to propagate error, but make
         // sure to close connections no matter what.
         InputStream response;
-        //String response;
         try {
             method.setRequestHeader("Content-Type", "text/xml;charset=utf-8");
             byte[] b = soapXML.getBytes();
             method.setRequestHeader("Content-Length", String.valueOf(b.length));
             method.setRequestEntity(new ByteArrayRequestEntity(b));
             client.executeMethod(method);
-            
             response = method.getResponseBodyAsStream();
-            //response = method.getResponseBodyAsString();
+
+            //String responseString = method.getResponseBodyAsString();
             //if(logger.isDebugEnabled()) {
-            //  logger.debug("String response is: " + response);
+            //  logger.debug("String response is: " + responseString);
             //}
 
             try {
@@ -89,6 +126,7 @@ public class EcsSoap {
                 digester.setNamespaceAware(true);
                 digester.parse(response);
             } catch (Exception exc) {
+                logger.error(getClass().toString() + exc.getMessage());
                 exc.printStackTrace();
             } finally {
                 response.close();
@@ -97,24 +135,36 @@ public class EcsSoap {
             method.releaseConnection();
         }
 
-        if(logger.isDebugEnabled()) {
-            Iterator <Object> exchangeIterator =
+        if (logger.isDebugEnabled()) {
+            Iterator < Object > exchangeIterator =
                 this.getExchangeObjects().iterator();
-            while(exchangeIterator.hasNext()) {
-               logger.debug("Element toString " + exchangeIterator.next().toString());
+            while (exchangeIterator.hasNext()) {
+               logger.debug("Element toString " + exchangeIterator.next().
+                       toString());
             }
         }
 
     }
     //This needs to be public to work with Digester File object add injection
-    public void addExchangeObject(Object obj) {
+    /**
+     * Add a java object constructed of the soap elements returned to the queue.
+     * @param obj parameter representing a record
+     */
+    public final void addExchangeObject(final Object obj) {
         exchangeObjects.add(obj);
     }
 
-    private ConcurrentLinkedQueue<Object> exchangeObjects =
-        new ConcurrentLinkedQueue<Object>();
+    /**
+     * private ConcurrentLinkedQueue of exchange objects.
+     */
+    private ConcurrentLinkedQueue < Object > exchangeObjects =
+        new ConcurrentLinkedQueue < Object >();
 
-    public ConcurrentLinkedQueue <Object> getExchangeObjects() {
+    /**
+     * Get the queue of built up exchange domain objects.
+     * @return queue of exchange objects.
+     */
+    public final ConcurrentLinkedQueue < Object > getExchangeObjects() {
         return exchangeObjects;
     }
 
